892d040e29e18810bdb2cdef8caeb7c2022dd747
David A. Wheeler <dwheeler@dwheeler.com>
2018-01-23 10:31:35 -0500

    Add the "hello.py" trivial program.
    Signed-off-by: David A. Wheeler <dwheeler@dwheeler.com>

---
+++ hello.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+"""This is a trivial module to say 'Hello, world!'"""
+
+def print_hello():
+    """Say 'Hello, world!'"""
+    print("Hello, World!")
+
+if __name__ == "__main__":
+    print_hello()
